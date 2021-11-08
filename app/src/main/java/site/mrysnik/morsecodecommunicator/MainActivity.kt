c984c1eb57ad1044efccd0b07303a5a1e4e0c7c3
Oskar Swierczek <oswierczek99@gmail.com>
2021-11-08 21:51:03 +0100

    WIP Filter

--- app/src/main/java/site/mrysnik/morsecodecommunicator/MainActivity.kt
+++ app/src/main/java/site/mrysnik/morsecodecommunicator/MainActivity.kt
@@ -18,7 +18,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        SignalProcessor.getInstance().process()
+        //SignalProcessor.getInstance().process()
+        SignalProcessor.getInstance()
 
         val audioFilePath = Environment.getExternalStorageDirectory().absolutePath + "/Music/recording.mp3"
 
--- app/src/main/java/site/mrysnik/morsecodecommunicator/SignalProcessor.kt
+++ app/src/main/java/site/mrysnik/morsecodecommunicator/SignalProcessor.kt
@@ -1,25 +1,30 @@
 package site.mrysnik.morsecodecommunicator
 
 import com.matthewrussell.trwav.readIntLE
+import uk.me.berndporr.iirj.Butterworth
 import java.io.File
 import java.io.RandomAccessFile
+import kotlin.concurrent.thread
+import kotlin.math.absoluteValue
 
 @ExperimentalUnsignedTypes
-class SignalProcessor(audioFs: Int, wpm: Float): Parameters() {
-    private var audioData: ByteArray = ByteArray(0)
-    private val delta = 1u
-    private val unitSignalLength = (this.samplingRate?.times(unit_len!!))?.toInt()?.div(1000)
-    private val streakFraction = 0.9
-
+class SignalProcessor(audioFs: Int, wpm: Float): Parameters(wpm = 20f) {
     // Ths companion object implements singleton logic for this class
     companion object {
         private var instance: SignalProcessor? = null
-        fun getInstance(audioFs: Int = samplingRate, wpm: Float = default_wpm): SignalProcessor {
+        fun getInstance(audioFs: Int = Parameters.samplingRate, wpm: Float = Parameters.default_wpm): SignalProcessor {
             if (this.instance != null)
                 return this.instance!!
             return SignalProcessor(audioFs, wpm)
         }
     }
+
+    private var audioData: ByteArray = ByteArray(0)
+    //private val delta = 0.03
+    private val delta = 1
+    private val unitSignalLength = (Parameters.samplingRate * unit_len!!).toInt()/1000
+    private val streakFraction = 0.9
+    private val processor: Thread = thread(true, false, null, "Signal processor", -1) { this.process() }
 
     fun process() {
         // mocked data - beginning
@@ -30,18 +35,47 @@ class SignalProcessor(audioFs: Int, wpm: Float): Parameters() {
         file.read(audioData)
         // mocked data - ending
 
-        // TODO Bandpass filter
+        var audioData2: MutableList<Double> = mutableListOf()
 
-        val ml = quantize(audioData)
+        audioData.forEach {
+            audioData2.add(it.toDouble())
+        }
 
-        val test2 = numStreak(ml)
+        //println(audioData2)
+        //val step1 = filter(audioData2)
+        val step2 = digitize(audioData2)
+        //val step2 = digitize(step1)
+        //println(step2)
+        val step3 = numStreak(step2)
+        var test: MutableList<Int> = mutableListOf()
+        step3.forEach {
+            test.add(it[1])
+        }
+        println(test)
 
-        // TODO Better synchronisation
-        test2.forEach { if(it[0] == 1) it[0] =0 else it[0] = 1 }
+        println(test.sorted().takeLast(100))
 
-        val symbols = num2symbols(test2)
-        println(symbols2char(symbols))
+        test = mutableListOf()
+        step3.forEach {
+            test.add(it[0])
+        }
+        //println(test)
+        val step4 = reduceShortStreaks(step3)
 
+        test = mutableListOf()
+        step4.forEach {
+            test.add(it[1])
+        }
+        println(test)
+
+        test = mutableListOf()
+        step4.forEach {
+            test.add(it[0])
+        }
+        println(test)
+        val step5 = num2symbols(step4)
+        println(step5)
+        println(symbols2char(step5))
     }
 
     private fun numStreak(list: List<Int>): List<IntArray> {
@@ -56,13 +90,14 @@ class SignalProcessor(audioFs: Int, wpm: Float): Parameters() {
                 num = intArrayOf(it, 1)
             }
         }
-        return reduceShortStreaks(numberStreaks)
+        return numberStreaks
     }
 
     private fun reduceShortStreaks(list: List<IntArray>): List<IntArray> {
         val numberStreaks: MutableList<IntArray> = mutableListOf()
         var ones = 0; var zeroes = 0 ; var sum = 0
         val limit = streakFraction * unitSignalLength!!
+
 
         list.forEach {
             sum += it[1]
@@ -72,6 +107,17 @@ class SignalProcessor(audioFs: Int, wpm: Float): Parameters() {
                 numberStreaks.add(intArrayOf(num, sum))
                 ones = 0; zeroes = 0; sum = 0
             }
+
+
+/*
+            sum += it[1]
+            if (it[1] < 500 || sum < limit) {
+                if(it[0] == 0) zeroes += it[1] else ones += it[1]
+            } else {
+                val num = if (zeroes > ones) 0 else 1
+                numberStreaks.add(intArrayOf(num, sum))
+                ones = 0; zeroes = 0; sum = 0
+            }*/
         }
 
         return numberStreaks
@@ -108,17 +154,30 @@ class SignalProcessor(audioFs: Int, wpm: Float): Parameters() {
     }
 
     @ExperimentalUnsignedTypes
-    private fun quantize(audioData: ByteArray): List<Int> {
+    private fun digitize(audioData: List<Double>): List<Int> {
         val quantizedData: MutableList<Int> = mutableListOf()
 
         audioData.toList().forEach {
-            if(it.toUByte() > 128u - delta && it.toUByte() < 128u + delta){
-                quantizedData.add(1)
-            } else {
+            if(it > -128.0 - delta && it < -128.0 + delta){
+            //if(it > -delta && it < delta){
                 quantizedData.add(0)
+            } else {
+                quantizedData.add(1)
             }
         }
 
         return quantizedData
+    }
+
+    private fun filter(audioData: List<Double>): List<Double> {
+        val butterworth = Butterworth()
+        butterworth.bandPass(200, Parameters.samplingRate.toDouble(), 1000.0,500.0)
+
+        val filtered: MutableList<Double> = mutableListOf()
+        audioData.forEach {
+            val test = butterworth.filter(it)
+            filtered.add(test)
+        }
+        return filtered
     }
 }
